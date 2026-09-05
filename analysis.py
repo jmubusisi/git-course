322973d72f66e07c189912c076cfe47e089734c3
Japhat Mubusisi <3Beleggings@gmail.com>
2026-09-05 12:41:01 +0200

    Update analysis

--- analysis.py
+++ analysis.py
@@ -2,4 +2,7 @@ print("Git and GitHub course")
 print ("Learning version control")
 print("My repository is connected to GitHub")
 print("Testing Git Pull")
+print("Version A")
+print("Version B")
+
 
