046645c9554c64e75f3077e1760195ba0be88e6f
Japhat Mubusisi <3Beleggings@gmail.com>
2026-09-05 19:53:22 +0200

    Improve mean input validation message

--- calculator.py
+++ calculator.py
@@ -1,24 +1,35 @@
 import statistics
 
+#-----------------------------------------------------
+# Mean Calculations
+#-----------------------------------------------------
 def mean(values):
     if not values:
-        raise ValueError("Values cannot be empty")
+        raise ValueError("Atleast one value is required")
     
     return sum(values)/len(values)
+
+#-------------------------------------------------------
+# Max Value
+#-------------------------------------------------------
 
 def maximum(values):
     if not values:
         raise ValueError("Values cannot be empty")
     return maximum(values)
 
+#---------------------------------------------------------
+# Data Range
+#---------------------------------------------------------
+
 def data_range(values):
     if not values:
         raise ValueError("Values cannot be empty")
     return max(values) - min(values)
 
-# ------------------------------------------#
-#Create Standard Deviation Calculation
-#--------------------------------------------#
+# -------------------------------------------------
+# Standard Deviation Calculation
+#--------------------------------------------------
 
 def standard_deviation(values):
     if len(values) < 2:
