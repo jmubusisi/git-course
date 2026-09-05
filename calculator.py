329ea97d23a092a8616f73f7318491189a0e6631
Japhat Mubusisi <3Beleggings@gmail.com>
2026-09-05 14:34:02 +0200

    Add range calculation

--- calculator.py
+++ calculator.py
@@ -8,3 +8,8 @@ def maximum(values):
     if not values:
         raise ValueError("Values cannot be empty")
     return maximum(values)
+
+def data_range(values):
+    if not values:
+        raise ValueError("Values cannot be empty")
+    return max(values) - min(values)
